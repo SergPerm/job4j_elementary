841875ef01fadde2c1f71eccf4f116ccf4bb81ef
Serg Mokhin <svmokhin@gmail.com>
2021-03-09 22:33:20 +0500

    5.7. Ипотека [#483331]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+
+    public static int year(int amount, int salary, double percent) {
+        int year = 1;
+        while ((amount + amount * percent/100) >= salary){
+            amount = (int)((amount + amount * percent/100) - salary);
+            year++;
+        }
+        return year;
+    }
+}
