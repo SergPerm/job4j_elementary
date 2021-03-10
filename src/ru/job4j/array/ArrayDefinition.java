25dffb4bea6afc2b9bc008f5d3b61fcc66350662
Serg Mokhin <svmokhin@gmail.com>
2021-03-10 18:31:18 +0500

    6.0.2. Заполнение массива. [#483253

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,15 @@ public class ArrayDefinition {
         System.out.println("Размер массива ages равен: " + ages.length);
         System.out.println("Размер массива surnames равен: " + surnames.length);
         System.out.println("Размер массива prices равен: " + prices.length);
+        System.out.println();
+        String[] names = new String[4];
+        names[0] = "Serg Mokhin";
+        names[1] = "Petr Arsentev";
+        names[2] = "Stas Korobeinikov";
+        names[3] = "Vova Putin";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
