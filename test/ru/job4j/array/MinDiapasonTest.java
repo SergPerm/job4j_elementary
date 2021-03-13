390ad592a13dfd519d92207c63c77c95365915bf
Serg Mokhin <svmokhin@gmail.com>
2021-03-13 14:20:14 +0500

    6.6.1. Двухмерный массив. Вывод на консоль. [#483259]

--- test/ru/job4j/array/MinDiapasonTest.java
+++ test/ru/job4j/array/MinDiapasonTest.java
@@ -10,7 +10,7 @@ public class MinDiapasonTest {
     @Test
     public void whenFirstMin() {
         assertThat(
-                MinDiapason.findMin(new int[] {-1, 0, 5, 10},1, 3),
+                MinDiapason.findMin(new int[] {-1, 0, 5, 10}, 1, 3),
                 is(0)
         );
     }
@@ -18,7 +18,7 @@ public class MinDiapasonTest {
     @Test
     public void whenLastMin() {
         assertThat(
-                MinDiapason.findMin(new int[] {10, 5, 3, 1},1, 3),
+                MinDiapason.findMin(new int[] {10, 5, 3, 1}, 1, 3),
                 is(1)
         );
     }
@@ -26,7 +26,7 @@ public class MinDiapasonTest {
     @Test
     public void whenMiddleMin() {
         assertThat(
-                MinDiapason.findMin(new int[] {10, 2, 5, 1},0, 2),
+                MinDiapason.findMin(new int[] {10, 2, 5, 1}, 0, 2),
                 is(2)
         );
     }
